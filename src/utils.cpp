97da1dfdd42c15d7031f55b88b84cf2fffd97516
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2020-11-24 12:13:11 +0100

    ⚙ Implemented DFS tree and its PostOrder traversal

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,11 +1,10 @@
-#include<map>
-
+#include<stack>
 #include "utils.h"
 
 
 namespace DFS
 {
-    DFS::DFS(const Graph& g) : result(), g(g), entries(), colours(), timestamp(0)
+    DFS::DFS(const Graph& g) : result(), tree(), g(g), entries(), colours(), timestamp(0)
     {
         // initialize node colours
         auto node = g.firstNode();
@@ -31,16 +30,35 @@ namespace DFS
         while ((node = node->succ()));
     }
 
+    void DFS::NodesPostOrder(std::list<int>& nodes)
+    {
+        std::stack<int> stack{{-1}};
+        do {
+            if (tree[stack.top()].empty()) {
+                nodes.push_back(stack.top());
+                stack.pop();
+                continue;
+            }
+
+            auto reverse_iterator = tree[stack.top()].rbegin();
+            for (; reverse_iterator != tree[stack.top()].rend(); ++reverse_iterator)
+                stack.push(*reverse_iterator);
+        }
+        while (!stack.empty());
+    }
+
     int DFS::StartNode(node v)
     {
         colours[v->index()] = GRAY;
+        tree[v->index()] = std::list<int>();
         return timestamp++;
     }
 
-    void DFS::FinishNode(node v, int timestamp_started)
+    void DFS::FinishNode(node v, int timestamp_started, node parent)
     {
         colours[v->index()] = BLACK;
         entries.push_front({timestamp_started, timestamp++, v});
+        tree[parent ? parent->index() : -1].push_back(v->index());
     }
 
     void DFS::Visit(node v)
@@ -58,7 +76,7 @@ namespace DFS
             // visit node and recursively its adjacent nodes
             Visit(adjNode);
             // mark node as finished
-            FinishNode(adjNode, DFI_in);
+            FinishNode(adjNode, DFI_in, v);
         }
     }
 }
--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<list>
+#include<map>
 #include<ogdf/basic/Graph.h>
  
 using namespace ogdf;
@@ -9,7 +10,7 @@ namespace DFS
     struct Entry {
         const int DFI_in;
         const int DFI_out;
-        const node vertex;
+        const node v;
     };
     typedef struct Entry Entry;
 
@@ -19,17 +20,20 @@ namespace DFS
     {
     public:
         std::list<Entry> result;
+        std::map<int, std::list<int>> tree;
 
         DFS(const Graph& g);
+
+        void NodesPostOrder(std::list<int>& nodes);
     
     protected:
-	    const Graph& g;
+        const Graph& g;
         std::list<Entry> entries;
         std::map<int, Colour> colours;
         int timestamp;
 
         int StartNode(node v);
-        void FinishNode(node v, int timestamp_started);
+        void FinishNode(node v, int timestamp_started, node parent = nullptr);
 
         void Visit(node v);
     };
