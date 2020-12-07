c090951ac73d90d9a5ac589524d8fb1de8c4b92a
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2020-12-07 16:43:51 +0100

    🔧 DFS algorithm bugfixes and improvements - Fixed DFS for directed graphs - Improved DFS in general

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,82 +1,106 @@
-#include<stack>
+#include<limits>
 #include "utils.h"
 
 
 namespace DFS
 {
-    DFS::DFS(const Graph& g) : result(), tree(), g(g), entries(), colours(), timestamp(0)
+    DFS::DFS(const GraphAttributes& ga) : nodes_pre(), nodes_post(), pre_order(), post_order(), edge_types(), tree(), G(ga.constGraph()), GA(ga), colours(), timestamp(0)
     {
         // initialize node colours
-        auto node = g.firstNode();
-        do {
-            colours[node->index()] = WHITE;
+        for (auto n : this->G.nodes) {
+            colours[n->index()] = WHITE;
+            pre_order[n->index()] = std::numeric_limits<int>::max();
+            post_order[n->index()] = std::numeric_limits<int>::max();
         }
-        while ((node = node->succ()));
         
         // start creation of DFS trees
-        node = g.firstNode();
-        do {
+        for (auto n : this->G.nodes) {
             // only visit unprocessed nodes
-            if (colours[node->index()] != WHITE)
+            if (colours[n->index()] != WHITE)
                 continue;
             
             // mark node as started
-            const int DFI_in = StartNode(node);
+            StartNode(n);
             // visit node and recursively its adjacent nodes
-            Visit(node);
+            Visit(n);
             // mark node as finished
-            FinishNode(node, DFI_in);
+            FinishNode(n);
         }
-        while ((node = node->succ()));
     }
 
-    void DFS::NodesPostOrder(std::list<int>& nodes)
+    void DFS::StartNode(node v)
     {
-        std::stack<int> stack{{-1}};
-        do {
-            if (tree[stack.top()].empty()) {
-                nodes.push_back(stack.top());
-                stack.pop();
-                continue;
-            }
-
-            auto reverse_iterator = tree[stack.top()].rbegin();
-            for (; reverse_iterator != tree[stack.top()].rend(); ++reverse_iterator)
-                stack.push(*reverse_iterator);
-        }
-        while (!stack.empty());
-    }
-
-    int DFS::StartNode(node v)
-    {
+        // right before visit - set target node to gray as processing will start
         colours[v->index()] = GRAY;
+        // generate pre-order index
+        pre_order[v->index()] = timestamp++;
+        // create list of child nodes
         tree[v->index()] = std::list<int>();
-        return timestamp++;
+        // save this node as processed in pre-order
+        nodes_pre.push_back(v);
     }
 
-    void DFS::FinishNode(node v, int timestamp_started, node parent)
+    void DFS::FinishNode(node v, node parent)
     {
+        // right after visit - set target node to black as processing has been finished
         colours[v->index()] = BLACK;
-        entries.push_front({timestamp_started, timestamp++, v});
+        // generate post-order index
+        post_order[v->index()] = timestamp++;
+        // append to parent's list of child nodes
         tree[parent ? parent->index() : -1].push_back(v->index());
+        // save this node as processed in post-order
+        nodes_post.push_back(v);
     }
 
     void DFS::Visit(node v)
     {
         adjEntry adj;
-        // visit nodes directly adjacent to this node
+        // walk through edges connected to this node
         for(adj = v->firstAdj(); adj; adj = adj->succ()) {
-            auto adjNode = adj->twinNode();
-            // only visit unprocessed nodes
-            if (colours.at(adjNode->index()) != WHITE)
+            // only work with source edges
+            if (this->GA.directed() && !adj->isSource())
                 continue;
+
+            // get target node
+            auto adjNode = adj->twinNode();
+            // get target node's current color
+            auto adjNodeColor = colours.at(adjNode->index());
             
-            // mark node as started
-            const int DFI_in = StartNode(adjNode);
-            // visit node and recursively its adjacent nodes
-            Visit(adjNode);
-            // mark node as finished
-            FinishNode(adjNode, DFI_in, v);
+            // check
+            if (adjNodeColor == WHITE) {
+                // target node is white - it has just been discovered now
+                // TREE EDGE
+                edge_types[adj->index()] = TREE;
+
+            } else if (adjNodeColor == GRAY) {
+                // target node is gray - it is unfinished predecessor of this node
+                // BACK EDGE - this closes cycle to its predecessor
+                edge_types[adj->index()] = BACK;
+
+            } else if (adjNodeColor == BLACK) {
+                // target node is black - it has already been finished
+                if (pre_order[adjNode->index()] > pre_order[v->index()]
+                    && post_order[adjNode->index()] < post_order[v->index()]) {
+                    // FORWARD EDGE iff target.DFI_in > my.DFI_in
+                    //   && target.DFI_out < my.DFI_out
+                    edge_types[adj->index()] = FORWARD;
+
+                } else {
+                    // CORSS EDGE else
+                    edge_types[adj->index()] = CROSS;
+
+                }
+            }
+
+            // only visit unprocessed nodes
+            if (adjNodeColor == WHITE) {
+                // mark node as started
+                StartNode(adjNode);
+                // visit node and recursively its adjacent nodes
+                Visit(adjNode);
+                // mark node as finished
+                FinishNode(adjNode, v);
+            }
         }
     }
 }
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,18 @@
 #include<list>
 #include<map>
 #include<ogdf/basic/Graph.h>
+#include<ogdf/basic/GraphAttributes.h>
+
+// Debug Macros
+#define DEBUG_ENABLED 1
+
+#ifdef DEBUG_ENABLED
+#define DEBUG(...) fprintf(stderr, __VA_ARGS__)
+#define DEBUG_EXPR(x) do { std::cerr << #x << ": " << x << std::endl; } while (0)
+#else 
+#define DEBUG(x) do {} while (0)
+#define DEBUG_EXPR(x) do {} while (0)
+#endif
  
 using namespace ogdf;
 
@@ -13,27 +25,36 @@ namespace DFS
         const node v;
     };
     typedef struct Entry Entry;
+    
+    typedef std::pair<node, node> NodePair;
 
-    enum Colour {WHITE, GRAY, BLACK};
+    enum NodeColour {WHITE, GRAY, BLACK};
+
+    enum EdgeType {TREE, BACK, FORWARD, CROSS};
 
     class DFS
     {
     public:
-        std::list<Entry> result;
+        std::vector<node> nodes_pre;
+        std::vector<node> nodes_post;
+
+        std::map<int, int> pre_order;
+        std::map<int, int> post_order;
+
+        std::map<int, EdgeType> edge_types;
         std::map<int, std::list<int>> tree;
 
-        DFS(const Graph& g);
-
-        void NodesPostOrder(std::list<int>& nodes);
+        DFS(const GraphAttributes& ga);
     
     protected:
-        const Graph& g;
-        std::list<Entry> entries;
-        std::map<int, Colour> colours;
+        const Graph& G;
+        const GraphAttributes& GA;
+
+        std::map<int, NodeColour> colours;
         int timestamp;
 
-        int StartNode(node v);
-        void FinishNode(node v, int timestamp_started, node parent = nullptr);
+        void StartNode(node v);
+        void FinishNode(node v, node parent = nullptr);
 
         void Visit(node v);
     };
